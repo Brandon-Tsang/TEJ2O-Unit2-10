6d7f01c40c5134e9875e49e1603c2b5ae2cab8fb
Brandon-Tsang <brandon.tsang@stu.ocsb.ca>
2026-03-13 13:26:36 -0400

    Show LED levels

--- main.ts
+++ main.ts
@@ -1,8 +1,41 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Brandon
+ * Created on: Feb 2026
+ * This program will randomly turn on neopixels
 */
 
-basic.showString('Hello, World!')
+// variables
+let lightValue = 0
+let neopixelStrip: neopixel.Strip = null
+
+// setup
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+// Show Neopixel
+input.onButtonPressed(Button.A, function () {
+    neopixelStrip.clear()
+    lightValue = input.lightLevel()
+    if (lightValue > 52) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    }
+
+    if (lightValue > 104) {
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    }
+
+    if (lightValue > 156) {
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    }
+
+    if (lightValue > 208) {
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    }
+    neopixelStrip.show()
+})
